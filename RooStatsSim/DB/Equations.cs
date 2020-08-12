9d6c37aad6d12970cec24131a7c32d7a86cb1b26
jooss287 <jooss287@gmail.com>
2020-08-12 16:05:53 +0900

    [edit] damage equation, complete damage equation without increse damage card

--- RooStatsSim/DB/Equations.cs
+++ RooStatsSim/DB/Equations.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 
 namespace RooStatsSim.DB
 {
+    enum CALC_STANDARD
+    {
+        NONE,
+        MIN_DAMAGE,
+        MAX_DAMAGE
+    }
     class Equations
     {
         public Equations(int param_attack_type, Status param_status, ItemAbility param_abilitys)
@@ -24,33 +30,53 @@ namespace RooStatsSim.DB
         const double RANDOM_ATK_WEIGHT = 0.05;
         const int BASE_ATK = 0;
         const int BASE_MATK = 5;
+        public double element_inc = 0;
+        public double size_panelty = 1;
 
-        double GetStrATK()
-        {
-            return abilities.ATK_weapon * status._str * STR_WEIGHT;
-        }
-        double GetRandomATK()
-        {
-            return abilities.ATK_weapon * RANDOM_ATK_WEIGHT;
-        }
+        double GetStrATK()              { return abilities.ATK_weapon * status._str * STR_WEIGHT; }
+        double GetDexATK()              { return abilities.ATK_weapon * status._dex * STR_WEIGHT; }
+        double GetRandomATK()           { return abilities.ATK_weapon * RANDOM_ATK_WEIGHT; }
 
-        public int CalcATKdamage()
+        public int CalcATKdamage(CALC_STANDARD calc_standard = CALC_STANDARD.NONE)
         {
             double status_atk;
+            double tot_weapon_atk;
+            double randomATK = 0;
+
+            switch (calc_standard)
+            {
+                case CALC_STANDARD.NONE:
+                    randomATK = 0;
+                    break;
+                case CALC_STANDARD.MIN_DAMAGE:
+                    randomATK = -1 * GetRandomATK();
+                    break;
+                case CALC_STANDARD.MAX_DAMAGE:
+                    randomATK = 1 * GetRandomATK();
+                    break;
+            }
+
             if (attack_type == 0)
+            {
                 status_atk = BASE_ATK + status._str + (status._dex + status._luk) * 0.2 + status._base * 0.25;
+                tot_weapon_atk = abilities.ATK_weapon + abilities.ATK_smelting + GetStrATK() + randomATK;
+            }
             else
+            {
                 status_atk = BASE_ATK + status._dex + (status._str + status._luk) * 0.2 + status._base * 0.25;
+                tot_weapon_atk = abilities.ATK_weapon + abilities.ATK_smelting + GetDexATK() + randomATK;
+            }
             status_atk *= 2;
 
-            double tot_weapon_atk = abilities.ATK_weapon + abilities.ATK_smelting + GetStrATK();
-            double tot_weapon_atk_min = tot_weapon_atk - GetRandomATK();
-            double tot_weapon_atk_max = tot_weapon_atk + GetRandomATK();
-            double tot_weapon_atk_inc = tot_weapon_atk /** SIZE_PANELTY*/;
+            double tot_weapon_atk_ratio = (0.01 * size_panelty);
+            double tot_weapon_atk_inc = tot_weapon_atk * tot_weapon_atk_ratio;
 
             double tot_equip_atk = tot_weapon_atk_inc + abilities.ATK_equipment;
-            double tot_equip_atk_inc = tot_equip_atk * (1 + 0.01*abilities.element_increse) * (1 + 0.01*abilities.tribe_increse)
+
+            double tot_equip_atk_ratio = (1 + element_inc) * (1 + 0.01*abilities.element_increse) * (1 + 0.01*abilities.tribe_increse)
                 * (1 + 0.01*abilities.size_increse) * (1 + 0.01*abilities.boss_increse) * (1 + 0.01*abilities.ATK_percent);
+            double tot_equip_atk_inc = tot_equip_atk * tot_equip_atk_ratio;
+            
 
             double tot_atk = status_atk + abilities.ATK_mastery + tot_equip_atk_inc;
             double tot_atk_inc = tot_atk * (1 + 0.01*abilities.PDamage_percent) + abilities.PDamage_addition;
