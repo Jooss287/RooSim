e8c2456e7c35cb290ce733d2ac3132a590941011
jooss287 <jooss287@gmail.com>
2020-08-13 00:49:45 +0900

    [fix] bugfix and edit equation

--- RooStatsSim/DB/Equations.cs
+++ RooStatsSim/DB/Equations.cs
@@ -15,19 +15,21 @@ namespace RooStatsSim.DB
     }
     class Equations
     {
-        public Equations(int param_attack_type, Status param_status, ItemAbility param_abilitys)
+        public Equations(int param_attack_type, Status param_status, ItemAbility param_abilitys, MonsterDB param_mobDB)
         {
             attack_type = param_attack_type;
             status = param_status;
             abilities = param_abilitys;
+            mobDB = param_mobDB;
         }
 
         private readonly int attack_type;
         private readonly ItemAbility abilities;
         private readonly Status status;
+        private readonly MonsterDB mobDB;
 
         const double STR_WEIGHT = 0.005;
-        const double RANDOM_ATK_WEIGHT = 0.05;
+        const double RANDOM_ATK_WEIGHT = 0.1;
         const int BASE_ATK = 0;
         const int BASE_MATK = 5;
         public double element_inc = 0;
@@ -36,6 +38,12 @@ namespace RooStatsSim.DB
         double GetStrATK()              { return abilities.ATK_weapon * status._str * STR_WEIGHT; }
         double GetDexATK()              { return abilities.ATK_weapon * status._dex * STR_WEIGHT; }
         double GetRandomATK()           { return abilities.ATK_weapon * RANDOM_ATK_WEIGHT; }
+        double GetDefRatio()
+        {
+            double def_ignore = 1 - 0.01 * abilities.def_ignore;
+            double def_ratio = (4000 + (mobDB.defense * def_ignore)) / (4000 + (mobDB.defense * def_ignore * 10));
+            return def_ratio;
+        }
 
         public int CalcATKdamage(CALC_STANDARD calc_standard = CALC_STANDARD.NONE)
         {
@@ -74,12 +82,12 @@ namespace RooStatsSim.DB
             double tot_equip_atk = tot_weapon_atk_inc + abilities.ATK_equipment;
 
             double tot_equip_atk_ratio = (1 + element_inc) * (1 + 0.01*abilities.element_increse) * (1 + 0.01*abilities.tribe_increse)
-                * (1 + 0.01*abilities.size_increse) * (1 + 0.01*abilities.boss_increse) * (1 + 0.01*abilities.ATK_percent);
+                * (1 + 0.01*abilities.size_increse) * (1 + 0.01*abilities.boss_increse) * (1 + 0.01*abilities.ATK_percent) * GetDefRatio();
             double tot_equip_atk_inc = tot_equip_atk * tot_equip_atk_ratio;
             
 
             double tot_atk = status_atk + abilities.ATK_mastery + tot_equip_atk_inc;
-            double tot_atk_inc = tot_atk * (1 + 0.01*abilities.PDamage_percent) + abilities.PDamage_addition;
+            double tot_atk_inc = tot_atk * (1 + 0.01*abilities.PDamage_percent) * (1 + 0.01*abilities.PDamage_attack_type) + abilities.PDamage_addition;
 
             return Convert.ToInt32(Math.Floor(tot_atk_inc));
         }
