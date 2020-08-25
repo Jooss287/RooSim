e591f99bb35e82bf09bbe5597e06d121ef363ad9
jooss287 <jooss287@gmail.com>
2020-08-25 17:57:51 +0900

    [feat] buff_system(concentrate)

---
+++ RooStatsSim/Equation/Job/LoadKnight.cs
@@ -0,0 +1,33 @@
+﻿using RooStatsSim.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RooStatsSim.Equation.Job
+{
+    class LoadKnight : Equations
+    {
+        public LoadKnight(int param_attack_type, Status param_status, ItemAbility param_abilitys, MonsterDB param_mobDB)
+        : base(param_attack_type, param_status, param_abilitys, param_mobDB)
+        {
+        }
+
+        private bool buff_concentrate_lvl = true;
+        private double buff_concentrate_ratio = 0.25;
+        
+        protected override double GetBaseTotalATK(CALC_STANDARD calc_standard = CALC_STANDARD.NONE)
+        {
+            double base_total_atk = abilities.ATK_weapon + abilities.ATK_smelting + statusATK.GetStatusBonusATK() + base.GetRandomATK() * (int)calc_standard;
+            double buff_concentrate = Convert.ToInt32(buff_concentrate_lvl) * (abilities.ATK_weapon + statusATK.GetStatusBonusATK() + abilities.ATK_smelting) * buff_concentrate_ratio;
+            return base_total_atk + buff_concentrate;
+        }
+        protected override double TotalEquipATK(double total_weapon_atk)
+        {
+            double base_total_atk = total_weapon_atk + abilities.ATK_equipment;
+            double buff_concentrate = Convert.ToInt32(buff_concentrate_lvl) * (abilities.ATK_equipment) * buff_concentrate_ratio;
+            return base_total_atk + buff_concentrate;
+        }
+    }
+}
