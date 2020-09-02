8308ab4cfb832aa80c704513b22c9d42f42fc904
Jooss287 <Jooss287@gmail.com>
2020-09-03 01:11:09 +0900

    [fate] statis window binding

---
+++ RooStatsSim/UI/StatusClass.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RooStatsSim.UI
+{
+    class StatusInfo
+    {
+        string _name;
+        int _point;
+        int _add_point;
+
+        public StatusInfo(string name, int point, int add_point)
+        {
+            _name = name;
+            _point = point;
+            _add_point = add_point;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public int Point
+        {
+            get { return _point; }
+        }
+        public int AddPoint
+        {
+            get { return _add_point; }
+        }
+    }
+
+    class StatusList : ObservableCollection<StatusInfo>
+    {
+        public StatusList()
+        {
+            Add(new StatusInfo("STR", 10, 1));
+            Add(new StatusInfo("AGI", 10, 1));
+            Add(new StatusInfo("VIT", 10, 1));
+            Add(new StatusInfo("DEX", 10, 1));
+            Add(new StatusInfo("INT", 10, 1));
+            Add(new StatusInfo("LUK", 10, 1));
+        }
+    }
+
+
+    class BasicOptionInfo
+    {
+        string _name;
+        int _point;
+        int _add_point;
+
+        public BasicOptionInfo(string name, int point, int add_point)
+        {
+            _name = name;
+            _point = point;
+            _add_point = add_point;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public int Point
+        {
+            get { return _point; }
+        }
+        public int AddPoint
+        {
+            get { return _add_point; }
+        }
+    }
+
+    class BasicOptionInfoList : ObservableCollection<BasicOptionInfo>
+    {
+        public BasicOptionInfoList()
+        {
+            Add(new BasicOptionInfo("ATK", 10, 1));
+            Add(new BasicOptionInfo("MATK", 10, 1));
+            Add(new BasicOptionInfo("제련ATK", 10, 1));
+            Add(new BasicOptionInfo("제련MATK", 10, 1));
+            Add(new BasicOptionInfo("ASPD", 10, 1));
+            Add(new BasicOptionInfo("HIT", 10, 1));
+            Add(new BasicOptionInfo("DEF", 10, 1));
+            Add(new BasicOptionInfo("MDEF", 10, 1));
+            Add(new BasicOptionInfo("제련DEF", 10, 1));
+            Add(new BasicOptionInfo("제련MDEF", 10, 1));
+        }
+    }
+}
--- RooStatsSim/UI/StatusWindow.xaml.cs
+++ RooStatsSim/UI/StatusWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RooStatsSim.UI
 {
@@ -23,46 +11,6 @@ namespace RooStatsSim.UI
         public StatusWindow()
         {
             InitializeComponent();
-        }
-    }
-
-    class StatusInfo
-    {
-        string _name;
-        int _point;
-        int _add_point;
-
-        public StatusInfo(string name, int point, int add_point)
-        {
-            _name = name;
-            _point = point;
-            _add_point = add_point;
-        }
-
-        public string Name
-        {
-            get { return _name; }
-        }
-        public int Point
-        {
-            get { return _point; }
-        }
-        public int AddPoint
-        {
-            get { return _add_point; }
-        }
-    }
-
-    class StatusInit: ObservableCollection<StatusInfo>
-    {
-        public StatusInit()
-        {
-            Add(new StatusInfo("STR", 10, 1));
-            Add(new StatusInfo("AGI", 10, 1));
-            Add(new StatusInfo("VIT", 10, 1));
-            Add(new StatusInfo("DEX", 10, 1));
-            Add(new StatusInfo("INT", 10, 1));
-            Add(new StatusInfo("LUK", 10, 1));
         }
     }
 }
