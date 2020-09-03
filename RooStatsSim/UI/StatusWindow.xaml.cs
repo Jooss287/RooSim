d74f2ebd017094f638c6d2aca0bd5f8b0e7a3e6f
Blurrr <jooss287@gmail.com>
2020-09-03 15:49:29 +0900

    Feat/status window (#2)
    * [feat] Status Window binding complete

---
+++ RooStatsSim/UI/StatusWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using RooStatsSim.DB;
+using System.Windows;
+
+namespace RooStatsSim.UI
+{
+    /// <summary>
+    /// StatusWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class StatusWindow : Window
+    {
+        StatusList BindingStatus;
+        Status statusDB;
+        Status statusAddDB;
+
+        public StatusWindow()
+        {
+            //DB가 레퍼로 들어왔다 치고.
+            statusDB = new Status();
+            statusAddDB = new Status();
+
+            InitializeComponent();
+
+            BindingStatus = new StatusList(ref statusDB, ref statusAddDB);
+            StatusListBox.ItemsSource = BindingStatus;
+        }
+    }
+}
