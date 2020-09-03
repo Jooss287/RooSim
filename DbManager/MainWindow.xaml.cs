c1bd958e5747afa65af832bea9e9122bf9298800
jooss287 <jooss287@gmail.com>
2020-09-03 17:58:01 +0900

    [init] DB Manager Initialize

---
+++ DbManager/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace DbManager
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ManagerSelector_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton source = e.Source as RadioButton;
+            string contents = source.Content.ToString();
+
+            if (frame_contents != null)
+                if (string.Compare(contents, "ItemDB") == 0)
+                    frame_contents.Source = new Uri("/UI/ItemManager.xaml", UriKind.Relative);
+                else
+                    frame_contents.Source = new Uri("/UI/MonsterManager.xaml", UriKind.Relative);
+                  
+        }
+    }
+}
