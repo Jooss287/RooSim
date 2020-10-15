ca56af376ba09a8e40cff06c8f619e15adfde333
Jooss287 <jooss287@gmail.com>
2020-10-15 17:49:16 +0900

    bugfix job novice

--- RooStatsSim/Equation/Job/JobSelect.cs
+++ RooStatsSim/Equation/Job/JobSelect.cs
@@ -38,7 +38,7 @@ namespace RooStatsSim.Equation.Job
     {
         List<dynamic> Job = new List<dynamic>()
             {
-                new Equations(),
+                new Novice(),
                 new LordKnight(),
                 new WhiteSmith(),
                 new AssassinCross(),
