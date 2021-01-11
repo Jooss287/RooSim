eafe6b116a75378b181e997a19564a2b5be3d71b
Jooss287 <jooss287@gmail.com>
2021-01-11 17:14:42 +0900

    update Consumable Buff item, image&option

--- RooStatsSim/DB/ConsumableItem/PortionConsumable.cs
+++ RooStatsSim/DB/ConsumableItem/PortionConsumable.cs
@@ -13,6 +13,24 @@ namespace RooStatsSim.DB.ConsumableItem
         DEX,
         LUK,
     }
+    public enum ADVANCED_PORTION_CONSUMABLE_ITEM
+    {
+        STR_INT,
+        STR_AGI,
+        STR_DEX,
+        STR_VIT,
+        STR_LUK,
+        INT_AGI,
+        INT_DEX,
+        INT_VIT,
+        INT_LUK,
+        AGI_DEX,
+        AGI_VIT,
+        AGI_LUK,
+        DEX_VIT,
+        DEX_LUK,
+        LUK_VIT,
+    }
     class PortionConsumable
     {
         public static Dictionary<string, string> BASIC_PORTION_CONSUMABLE_ITEM_KOR = new Dictionary<string, string>()
@@ -23,6 +41,24 @@ namespace RooStatsSim.DB.ConsumableItem
             {Enum.GetName(typeof(BASIC_PORTION_CONSUMABLE_ITEM),BASIC_PORTION_CONSUMABLE_ITEM.INT), "지능 포션" },
             {Enum.GetName(typeof(BASIC_PORTION_CONSUMABLE_ITEM),BASIC_PORTION_CONSUMABLE_ITEM.DEX), "손재주 포션" },
             {Enum.GetName(typeof(BASIC_PORTION_CONSUMABLE_ITEM),BASIC_PORTION_CONSUMABLE_ITEM.LUK), "행운 포션" },
+        };
+        public static Dictionary<string, string> ADVANCED_PORTION_CONSUMABLE_ITEM_KOR = new Dictionary<string, string>()
+        {
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.STR_INT), "힘과 지능포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.STR_AGI), "힘과 민첩포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.STR_DEX), "힘과 손재주포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.STR_VIT), "힘과 체력포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.STR_LUK), "힘과 운포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.INT_AGI), "지능과 민첩포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.INT_DEX), "지능과 손재주포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.INT_VIT), "지능과 체력포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.INT_LUK), "지능과 운포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.AGI_DEX), "민첩과 손재주포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.AGI_VIT), "민첩과 체력포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.AGI_LUK), "민첩과 운포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.DEX_VIT), "손재주와 체력포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.DEX_LUK), "손재주와 운포션" },
+            {Enum.GetName(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM),ADVANCED_PORTION_CONSUMABLE_ITEM.LUK_VIT), "운과 체력포션" },
         };
         public Dictionary<string, ConsumableBuffInfo> Buff { get; set; }
 
@@ -37,6 +73,21 @@ namespace RooStatsSim.DB.ConsumableItem
                 {
                     ItemDB opt = new ItemDB();
                     opt.Option_ITYPE[name] = (i + 1) * 5;
+                    Buff[name].OPTION.Add(opt);
+                }
+            }
+
+            max_lvl = 1;
+            foreach (string name in Enum.GetNames(typeof(ADVANCED_PORTION_CONSUMABLE_ITEM)))
+            {
+                Buff.Add(name, new ConsumableBuffInfo(name, ADVANCED_PORTION_CONSUMABLE_ITEM_KOR[name], max_lvl));
+                string main_opt = name.Substring(0, 3);
+                string sub_opt = name.Substring(4, 3);
+                for (int i = 0; i < max_lvl; i++)
+                {
+                    ItemDB opt = new ItemDB();
+                    opt.Option_ITYPE[main_opt] = 15;
+                    opt.Option_ITYPE[sub_opt] = 7;
                     Buff[name].OPTION.Add(opt);
                 }
             }
