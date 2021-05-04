1a26839595d3dfc048a4c27eedc999cec917c4f8
Jooss287 <jooss287@gmail.com>
2021-05-04 17:58:46 +0900

    Initialize repectoring structure
     - Feat status point class
     - Feat basic ability class without any method

---
+++ RooStatsSim/DB/Abilitys/BasicAbility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RooStatsSim.DB.Abilitys
+{
+    public class BasicAbility
+    {
+        public StatusPoint Str { get; set; }
+        public StatusPoint Agi { get; set; }
+        public StatusPoint Vit { get; set; }
+        public StatusPoint Int { get; set; }
+        public StatusPoint Dex { get; set; }
+        public StatusPoint Luk { get; set; }
+        
+    }
+}
---
+++ RooStatsSim/DB/Abilitys/StatusPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RooStatsSim.DB.Abilitys
+{
+    public class StatusPoint
+    {
+        public int _point = 0;
+        
+        public int Point {
+            get {
+                return _point;
+            }
+            set
+            {
+                if (value < 0)
+                    return;
+                _point = value;
+            }
+        }
+    }
+}
